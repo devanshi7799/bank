4eae8621ed6c49e48ff72a2a7262af567c68d31e
Devanshi  Shah <devanshishah@Devanshis-MacBook-Air.local>
2019-01-29 22:35:41 +0530

    Initial Commit

---
+++ bank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bank
+//
+//  Created by Devanshi  Shah on 29/01/19.
+//  Copyright © 2019 Devanshi  Shah. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
